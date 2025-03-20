fd5bc26a18d62120077202eb4747d9e76c1ad910
threadfinder950 <36114521+threadfinder950@users.noreply.github.com>
2025-03-20 14:28:38 -0500

    repaired path

--- TVIEW/client/src/components/events/EventsPage.tsx
+++ TVIEW/client/src/components/events/EventsPage.tsx
@@ -10,7 +10,7 @@ import {
 } from '@mui/material';
 import axios from 'axios';
 import EventEditor from '../forms/EventEditor';
-import { API } from 'src/config/api';
+import { API } from '../../config/api';
 
 const EventsPage = () => {
   const [events, setEvents] = useState([]);
